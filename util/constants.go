bb2434c271d58799dddf9be4278ac085ab4fa672
DexSlender <91853649+DexSlender@users.noreply.github.com>
2023-07-12 05:11:58 +0000

    add(colors): Hex colors

--- util/constants.go
+++ util/constants.go
@@ -11,3 +11,15 @@ const (
 	STRIKE            = "~~"
 	UNDERLINE         = "__"
 )
+
+const (
+	Green    = 0x00ff00
+	Red      = 0xff0000
+	Yellow   = 0xffff00
+	Blue     = 0x0000ff
+	Purple   = 0x7d00ff
+	Pink     = 0xff00ff
+	Magenta  = 0xff007d
+	DDark    = 0x2b2d31
+	DBlurple = 0x5865f2
+)
