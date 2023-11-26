913aba2b5c2d378d09af32b983bbaa45880c34fb
Eduardo Jav <eduarzamora340@gmail.com>
2023-11-26 03:36:54 +0000

    Random thing

--- commands/new.go
+++ commands/new.go
@@ -14,11 +14,16 @@ func (c *NewCmd) Init(r util.JHRegister) {
 	c.Description = "Create a new custom command"
 
 	r.Modal("command-maker", func(ctx *util.ModalCtx) error {
-		return ctx.CreateMessage(discord.NewMessageCreateBuilder().
-			SetContent("# Okay, but not working for now...").
-			SetEphemeral(true).
-			Build(),
-		)
+		o := util.Parse(ctx.Data.Text("code"))
+		for _, order := range o {
+			order(*ctx)
+		}
+		return nil
+		// ctx.CreateMessage(discord.NewMessageCreateBuilder().
+		// 	SetContent("# Okay, but not working for now...").
+		// 	SetEphemeral(true).
+		// 	Build(),
+		// )
 	})
 }
 
@@ -26,17 +31,18 @@ func (c *NewCmd) Run(ctx *util.JContext) error {
 	return ctx.Modal(discord.NewModalCreateBuilder().
 		SetCustomID("command-maker").
 		SetTitle("Command Maker").
+		// AddActionRow(discord.TextInputComponent{
+		// 	CustomID:    "name",
+		// 	Style:       discord.TextInputStyleShort,
+		// 	Label:       "Name",
+		// 	Placeholder: "ping, hello, greet",
+		// }).
 		AddActionRow(discord.TextInputComponent{
-			CustomID:    "name",
-			Style:       discord.TextInputStyleShort,
-			Label:       "Name",
-			Placeholder: "ping, hello, greet",
-		}).
-		AddActionRow(discord.TextInputComponent{
-			CustomID:    "code",
-			Style:       discord.TextInputStyleParagraph,
-			Label:       "Code",
-			Placeholder: "working on it\n>",
+			CustomID: "code",
+			Style:    discord.TextInputStyleParagraph,
+			Label:    "Code",
+			// Placeholder: "> hello",
+			Value: "> ",
 		}).
 		Build(),
 	)
